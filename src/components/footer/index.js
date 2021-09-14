c16056df7d450f7fc3d3aa7fbdde2c02b8a321ec
93114 <93114@enfi.com>
2021-09-14 10:24:48 +0800

    modify footer ui

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -36,18 +36,12 @@ export default class Footer extends Component {
                         </div>
                         <div className="footTitle small">
                             <span><a href="http://web.miningcloud.com.cn:8033" target='_blank'>矿业经济数据</a></span>
-                            <span><a href="http://da.miningcloud.com.cn" target='_blank'>数据分析平台</a></span>
+                            <span><a href="http://design.miningcloud.com.cn" target='_blank'>MIM工程设计云</a></span>
                             <span><a href="https://www.th-icloud.cn/" target='_blank'>天河工业云</a></span>
                             <span><span className="icon icon3"></span><span style={{ paddingLeft: '30px' }}>北京复兴路12号</span></span>
                         </div>
                         <div className="footTitle small">
                             <span><a href="http://equip.miningcloud.com.cn" target='_blank'>矿业装备数据</a></span>
-                            <span>人工智能</span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                        <div className="footTitle small">
-                            <span><a href="http://design.miningcloud.com.cn" target='_blank'>MIM工程设计云</a></span>
                             <span><a href="https://web.miningcloud.com.cn:8046/meeting" target='_blank'>视频会议</a></span>
                             <span></span>
                             <span></span>
@@ -55,12 +49,13 @@ export default class Footer extends Component {
                     </div>
                     <div className="wechat"><img src={require("../../img/wechat.jpg")} /><p>微信关注</p></div>
                 </div>
-                <div className="copyright">
+                <div className="copyright">                                                                                                                                                                                  
                     <div>
                         <span><a href="http://www.miningcloud.com.cn/admin" target='_blank'>后台管理</a>|<a href="http://doc.miningcloud.com.cn:8066" target='_blank'>矿业云网盘</a>|<a href="http://www.miningcloud.com.cn/container/register_rotocol" target='_blank'>免责申明</a></span>
                         <span>Copyright@版权所有</span>
+                        <span>中国恩菲工程技术有限公司</span>
                         <span>中国矿业信息化协同创新北京市工程研究中心</span>
-                        <span>网站备案号 京ICP备18055077号</span>
+                        <span>京ICP备18055077-1号</span>
                     </div>
                 </div>
             </div>
